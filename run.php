c4a1355c17e6c1e538a4a705520eb2e54872e3e7
BettaSplendid <forbiddenbetta@gmail.com>
2022-02-15 14:57:24 +0100

    Work

---
+++ run.php
@@ -0,0 +1,65 @@
+<?php
+
+
+
+define(
+    'IS_AJAX',
+    isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest'
+);
+
+if (!IS_AJAX) {
+    die('restricted access');
+}
+
+$file = isset($_FILES['file']['tmp_name']) ? $_FILES['file']['tmp_name'] : '';
+$responses = ['error' => 'false'];
+$file_name = $_POST['file_name'];
+
+if (isset($_POST['file'])) {
+    if ($_POST['file'] === 'undefined') {
+        $responses[] = 'nonewfiles';
+    }
+}
+
+if ($file !== '') {
+    if (0 < $_FILES['file']['error']) {
+        _addError();
+        $responses[] = 'Erreur d\'upload';
+    } else {
+        $authorized_format_file = [
+            "image/jpeg",
+            "image/jpg",
+        ];
+
+        if(!in_array($_FILES['file']["type"], $authorized_format_file)) {
+            $responses[] = 'Format Invalide';
+            _addError();
+        }
+
+        while(is_dir($folder_user = "vds_" . ((string) rand(10000, 990000) . '_'. time()))) {
+            $folder_user = "vds_" . ((string) rand(10000, 990000) . '_' . time());
+        }
+
+        $create_dir = mkdir($folder_user, 0755);
+    
+
+        if(move_uploaded_file($_FILES['file']['tmp_name'], $folder_user . '/' . $file_name)) {
+            $responses[] = 'Convert successfully';
+        } else {
+            $responses[] = 'Convert with errors';
+        }
+    }
+}
+
+if ($responses['error'] = 'false') {
+    unset($responses['error']);
+}
+
+print json_encode($responses);
+
+function _addError()
+{
+    $responses['error'] = 'true';
+    print json_encode($responses);
+    exit;
+}
